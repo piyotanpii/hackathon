761917e07b0305110bd3cb3d4b0d173d6b1d9df4
piyotanpii <chip.n.dale.0301@gmail.com>
2022-12-11 14:41:24 +0900

    add_aicon

---
+++ test-vue/supporters-hackathon/src/components/Footer.vue
@@ -0,0 +1,51 @@
+<template>
+	<v-footer
+      dark
+      padless
+    >
+      <v-card
+        flat
+        tile
+        class="accent white--text text-center"
+        color="#00B0FF"
+      >
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+          >
+            <v-icon size="30px">
+              {{ icon }}
+            </v-icon>
+            
+          </v-btn>
+        </v-card-text>
+
+        <v-card-subtitle class="white--text pt-0">
+          ---------------------------------------------------Connect with your Google Calendar!
+          By linking to Google, it will be possible to retrieve available time outside of the specified time.
+          Make scheduling easier.------------------------------------------------------------
+        </v-card-subtitle>
+      </v-card>
+    </v-footer>
+</template>
+
+<script>
+export default {
+	name: 'FooterArea',
+    data () {
+        return {
+            icons: [
+            'mdi-google',
+            ]}
+    }
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
